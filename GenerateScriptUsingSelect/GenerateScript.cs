132207cf4aca4d09d45d919725a6568412588cbd
Bhupendra Sharma <sharb11@aviva.co.uk>
2013-03-24 09:27:40 +0000

    Code commit

---
+++ GenerateScriptUsingSelect/GenerateScript.cs
@@ -0,0 +1,225 @@
+﻿namespace GenerateScriptUsingSelect
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+   
+
+    public class GenerateScript
+    {
+        private static string ApplicationId = "";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Application Id : ");
+            ApplicationId = Console.ReadLine();
+            Console.WriteLine("Application Id : " + ApplicationId);
+            Console.WriteLine("Please press enter to proceed");
+            Console.ReadLine();
+            ExecuteReader();
+        }
+
+        public static void ExecuteReader()
+        {
+            var myConnection = new SqlConnection("Data Source=echannel-nudquoteappcms-sy-sql.via.novonet;Initial Catalog=NUDQuoteAppCMS_SY;Integrated Security=SSPI;user id= SQLQuoteAppUser; password = y0g1834r;");
+            try
+            {
+                myConnection.Open();
+                TblCMSSectionsScript(myConnection);
+                TblFieldsScript(myConnection);
+                TblCMSFieldsScript(myConnection);
+                TblCMSPagesScript(myConnection);
+                TblCMSApplicationsScript(myConnection);
+                SelectDataFromTables();
+                
+            }
+            catch (Exception ex)
+            {
+                myConnection.Close();
+                Console.WriteLine("Exception Message : - " + ex.Message);
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+           
+        }
+
+        private static void TblCMSSectionsScript(SqlConnection myConnection)
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+
+            var myCommand = new SqlCommand("Select * from [tblCMSSections] WHERE [ApplicationId] = 'MyPolicy' and PageId NOT IN ('GeneralMotor', 'MotorHelp', 'AddDriver', 'VehEditHelp')", myConnection);
+            SqlDataReader myReader = myCommand.ExecuteReader();
+
+            StreamWriter wr = new StreamWriter("c:\\out.txt", false);
+
+            wr.WriteLine("\n");
+            wr.WriteLine("-- Starts of tblCMSSections Insert Script -- ");
+            wr.WriteLine("\n");
+            string deleteCommand = " DELETE FROM [NUDQuoteAppCMS_SY].[dbo].[tblCMSSections] WHERE [ApplicationId] = '" + ApplicationId + "'";
+            wr.WriteLine(deleteCommand);
+            wr.WriteLine("\n");
+            while (myReader.Read())
+            {
+                string insertCommand =
+                    " INSERT INTO [NUDQuoteAppCMS_SY].[dbo].[tblCMSSections]([ApplicationID],[PageID],[SectionID],[SectionDesc],[SectionOrder]) VALUES ('" + ApplicationId + "','" + myReader[1].ToString().Trim().Replace("'", "''") + "','" + myReader[2].ToString().Trim().Replace("'", "''") + "','" + myReader[3].ToString().Trim().Replace("'", "''") + "'," + myReader[4].ToString().Trim().Replace("'", "''") + ");";
+
+                wr.WriteLine(insertCommand);
+                Console.WriteLine(insertCommand);
+            }
+            wr.WriteLine("\n");            
+            wr.WriteLine("-------- Ends of tblCMSSections Insert Script -- ");
+            wr.Close();
+            myReader.Close();
+        }
+
+        private static void TblFieldsScript(SqlConnection myConnection)
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+
+            var myCommand = new SqlCommand("Select * from [tblFields] WHERE [ApplicationId] = 'MyPolicy' and PageId NOT IN ('GeneralMotor', 'MotorHelp', 'AddDriver', 'VehEditHelp')", myConnection);
+            SqlDataReader myReader = myCommand.ExecuteReader();
+
+            StreamWriter wr = new StreamWriter("c:\\out.txt", true);
+
+            wr.WriteLine("\n");
+            wr.WriteLine(" -------- Starts of tblFields Insert Script -- ");
+            wr.WriteLine("\n");
+            string deleteCommand = " DELETE FROM [NUDQuoteAppCMS_SY].[dbo].[tblFields] WHERE [ApplicationId] = '" + ApplicationId + "'";
+            wr.WriteLine(deleteCommand);
+            wr.WriteLine("\n");
+            while (myReader.Read())
+            {
+                string insertCommand =
+                    " INSERT INTO [NUDQuoteAppCMS_SY].[dbo].[tblFields]([FieldId],[PageId],[ApplicationId],[Label],[Text],[HelpId]) VALUES ('" + myReader[0].ToString().Trim().Replace("'", "''") + "','" + myReader[1].ToString().Trim().Replace("'", "''") + "','" + ApplicationId + "','" + myReader[3].ToString().Trim().Replace("'", "''") + "','" + myReader[4].ToString().Trim().Replace("'", "''") + "','" + myReader[5].ToString().Trim().Replace("'", "''") + "');";
+
+                wr.WriteLine(insertCommand);
+                Console.WriteLine(insertCommand);
+            }
+            wr.WriteLine("\n");            
+            wr.WriteLine("----- Ends of tblFields Insert Script -- ");
+            wr.Close();
+            myReader.Close();
+        }
+
+        private static void TblCMSFieldsScript(SqlConnection myConnection)
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+
+            var myCommand = new SqlCommand("Select * from [tblCMSFields] WHERE [ApplicationId] = 'MyPolicy' and PageId NOT IN ('GeneralMotor', 'MotorHelp', 'AddDriver', 'VehEditHelp')", myConnection);
+            SqlDataReader myReader = myCommand.ExecuteReader();
+
+            StreamWriter wr = new StreamWriter("c:\\out.txt", true);
+
+            wr.WriteLine("\n");
+            wr.WriteLine("------- Starts of tblCMSFields Insert Script -- ");
+            wr.WriteLine("\n");
+            string deleteCommand = " DELETE FROM [NUDQuoteAppCMS_SY].[dbo].[tblCMSFields] WHERE [ApplicationId] = '" + ApplicationId + "'";
+            wr.WriteLine(deleteCommand);
+            wr.WriteLine("\n");
+            while (myReader.Read())
+            {
+                string insertCommand =
+                    " INSERT INTO [NUDQuoteAppCMS_SY].[dbo].[tblCMSFields]([ApplicationID],[PageID],[SectionID],[FieldID],[FieldDesc],[FieldOrder],[HasLabel],[HasHelpLink],[HasText],[IsHeading],[IsMessage],[IsHTML]) VALUES ('" + ApplicationId + "','" + myReader[1].ToString().Trim().Replace("'", "''") + "','" + myReader[2].ToString().Trim().Replace("'", "''") + "','" + myReader[3].ToString().Trim().Replace("'", "''") + "','" + myReader[4].ToString().Trim().Replace("'", "''") + "'," + myReader[5].ToString().Trim().Replace("'", "''") + ",'" + myReader[6].ToString().Trim().Replace("'", "''") + "','" + myReader[7].ToString().Trim().Replace("'", "''") + "','" + myReader[8].ToString().Trim().Replace("'", "''") + "','" + myReader[9].ToString().Trim().Replace("'", "''") + "','" + myReader[10].ToString().Trim().Replace("'", "''") + "','" + myReader[11].ToString().Trim().Replace("'", "''") + "');";
+
+                wr.WriteLine(insertCommand);
+                Console.WriteLine(insertCommand);
+            }
+            wr.WriteLine("\n");            
+            wr.WriteLine("--------- Ends of tblCMSFields Insert Script -- ");
+            wr.Close();
+            myReader.Close();
+        }
+
+        private static void TblCMSPagesScript(SqlConnection myConnection)
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+
+            var myCommand = new SqlCommand("Select * from [tblCMSPages] WHERE [ApplicationId] = 'MyPolicy' and PageId NOT IN ('GeneralMotor', 'MotorHelp', 'AddDriver', 'VehEditHelp')", myConnection);
+            SqlDataReader myReader = myCommand.ExecuteReader();
+
+            StreamWriter wr = new StreamWriter("c:\\out.txt", true);
+
+            wr.WriteLine("\n");
+            wr.WriteLine("------- Starts of tblCMSPages Insert Script -- ");
+            wr.WriteLine("\n");
+            string deleteCommand = " DELETE FROM [NUDQuoteAppCMS_SY].[dbo].[tblCMSPages] WHERE [ApplicationId] = '" + ApplicationId + "'";
+            wr.WriteLine(deleteCommand);
+            wr.WriteLine("\n");
+            while (myReader.Read())
+            {
+                string insertCommand =
+                    " INSERT INTO [NUDQuoteAppCMS_SY].[dbo].[tblCMSPages]([ApplicationID],[PageID],[PageDesc],[PageOrder]) VALUES ('" + ApplicationId + "','" + myReader[1].ToString().Trim().Replace("'", "''") + "','" + myReader[2].ToString().Trim().Replace("'", "''") + "'," + myReader[3].ToString().Trim().Replace("'", "''") + ");";
+
+                wr.WriteLine(insertCommand);
+                Console.WriteLine(insertCommand);
+            }
+            wr.WriteLine("\n");
+            wr.WriteLine("--------- Ends of tblCMSPages Insert Script -- ");
+            wr.Close();
+            myReader.Close();
+        }
+
+        private static void TblCMSApplicationsScript(SqlConnection myConnection)
+        {
+            if (myConnection.State != System.Data.ConnectionState.Open)
+            {
+                myConnection.Open();
+            }
+
+            var myCommand = new SqlCommand("Select * from [tblCMSApplications] WHERE [ApplicationId] = 'MyPolicy'", myConnection);
+            SqlDataReader myReader = myCommand.ExecuteReader();
+
+            StreamWriter wr = new StreamWriter("c:\\out.txt", true);
+
+            wr.WriteLine("\n");
+            wr.WriteLine("------- Starts of tblCMSApplications Insert Script -- ");
+            wr.WriteLine("\n");
+            string deleteCommand = " DELETE FROM [NUDQuoteAppCMS_SY].[dbo].[tblCMSApplications] WHERE [ApplicationId] = '" + ApplicationId + "'";
+            wr.WriteLine(deleteCommand);
+            wr.WriteLine("\n");
+            while (myReader.Read())
+            {
+                string insertCommand =
+                    " INSERT INTO [NUDQuoteAppCMS_SY].[dbo].[tblCMSApplications]([ApplicationID],[ApplicationDesc]) VALUES ('" + ApplicationId + "','" + myReader[1].ToString().Trim().Replace("'", "''") + "');";
+
+                wr.WriteLine(insertCommand);
+                Console.WriteLine(insertCommand);
+            }
+            wr.WriteLine("\n");
+            wr.WriteLine("--------- Ends of tblCMSApplications Insert Script -- ");
+            wr.Close();
+            myReader.Close();
+        }
+
+        private static void SelectDataFromTables()
+        {
+            StreamWriter wr = new StreamWriter("c:\\out.txt", true);
+            wr.WriteLine("\n");
+            wr.WriteLine("------- Select statement script  from table -- ");
+            wr.WriteLine("\n");
+            wr.WriteLine(" SELECT * FROM [NUDQuoteAppCMS_SY].[dbo].[tblFields] WHERE [ApplicationId] = '" + ApplicationId + "'");
+            wr.WriteLine(" SELECT * FROM [NUDQuoteAppCMS_SY].[dbo].[tblCMSFields] WHERE [ApplicationId] = '" + ApplicationId + "'");
+            wr.WriteLine(" SELECT * FROM [NUDQuoteAppCMS_SY].[dbo].[tblCMSPages] WHERE [ApplicationId] = '" + ApplicationId + "'");
+            wr.WriteLine(" SELECT * FROM [NUDQuoteAppCMS_SY].[dbo].[tblCMSSections] WHERE [ApplicationId] = '" + ApplicationId + "'");
+            wr.WriteLine(" SELECT * FROM [NUDQuoteAppCMS_SY].[dbo].[tblCMSApplications] WHERE [ApplicationId] = '" + ApplicationId + "'");
+            wr.WriteLine("\n");
+            wr.WriteLine("--------- Ends of select statements script-- ");
+            wr.Close();
+        }
+    }
+}
---
+++ GenerateScriptUsingSelect/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GenerateScriptUsingSelect")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aviva")]
+[assembly: AssemblyProduct("GenerateScriptUsingSelect")]
+[assembly: AssemblyCopyright("Copyright © Aviva 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27a4079d-01c1-4ece-9918-2b855bcb11f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
